5e96e9692e5a09ce727e5875544db288bb992de6
Andrew Vo <voandrew.khoa@gmail.com>
2026-03-25 14:16:04 -0700

    Add APIs for creating slice views

--- lib/inc/slice.h
+++ lib/inc/slice.h
@@ -12,3 +12,11 @@ typedef struct slice_mutable {
     uint8_t *ptr;
     size_t len;
 } slice_mutable_t;
+
+static inline slice_mutable_t slice_mut_view(uint8_t *buffer, size_t len) {
+    return (slice_mutable_t) { .ptr = buffer, .len = len };
+}
+
+static inline slice_t slice_view(const uint8_t *buffer, size_t len) {
+    return (slice_t) { .ptr = buffer, .len = len };
+}
