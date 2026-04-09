8ef48a36c0b0e0fa7d8b9cc900984f03eb663907
atkvo <voandrew.khoa@gmail.com>
2026-04-09 15:44:29 -0700

    feat(app): update display content and cleanup display logic

--- app/src/tasks/task_display.c
+++ app/src/tasks/task_display.c
@@ -1,11 +1,15 @@
 #include "task_display.h"
+#include "antos.h"
 #include "framebuffer.h"
+#include "slice.h"
 
 #include <stdio.h>
 #include <string.h>
 
 static void splash(ssd1306_handle_t oled);
 static void write_number(slice_mutable_t s, size_t col, size_t row, size_t num);
+static void draw_stat(slice_mutable_t fb, uint8_t row, const char* label, uint32_t value);
+static ant_task_status_t task_init(task_display_ctx_t *ctx);
 
 task_display_ctx_t task_display_create_ctx(ssd1306_handle_t oled) {
     return (task_display_ctx_t) {
@@ -15,29 +19,33 @@ task_display_ctx_t task_display_create_ctx(ssd1306_handle_t oled) {
     };
 }
 
+
 ant_task_status_t task_display(task_display_ctx_t *ctx) {
     if (!ctx->initialized) {
-        ssd1306_init(ctx->disp);
-        ctx->count = 0;
-        ctx->initialized = true;
-        splash(ctx->disp);
-
-        fb_clear(ctx->disp->ram);
-        ant_task_schedule_next(1000);
-
-        return ANT_TASK_OK;
+        return task_init(ctx);
     }
 
-    write_number(ctx->disp->ram, 0, 0, ctx->count);
-    write_number(ctx->disp->ram, 0, 4, *ctx->press_count);
+    draw_stat(ctx->disp->ram, 0, "uptime", ant_ticks_to_ms(ant_get_tick_count()));
+    draw_stat(ctx->disp->ram, 1, "count", ctx->count);
+    draw_stat(ctx->disp->ram, 2, "press", *ctx->press_count);
 
     ctx->count++;
-    if (ctx->count >= 255) { ctx->count = 0; }
 
-    // should have a flag or something to update?
     ssd1306_update_nb(ctx->disp);
 
     ant_task_schedule_next(500);
+    return ANT_TASK_OK;
+}
+
+static ant_task_status_t task_init(task_display_ctx_t *ctx) {
+    ssd1306_init(ctx->disp);
+    ctx->count = 0;
+    ctx->initialized = true;
+    splash(ctx->disp);
+
+    fb_clear(ctx->disp->ram);
+    ant_task_schedule_next(1000);
+
     return ANT_TASK_OK;
 }
 
@@ -63,14 +71,23 @@ static void splash(ssd1306_handle_t oled) {
 
 static void write_number(slice_mutable_t s, size_t col, size_t row, size_t num) {
     enum {
-        MAX_DIGITS = 8,
-        MAX_VALUE = 9999,
+        MAX_DIGITS = 4,
+        MAX_VALUE = 999,
     };
-    char digits[MAX_DIGITS + 1];
-    if (num > (MAX_VALUE)) {
-        num = MAX_VALUE;
+    while (num > (MAX_VALUE)) {
+        num = num / MAX_VALUE;
     }
 
-    snprintf(digits, sizeof(digits), "%08d", num);
+    char digits[MAX_DIGITS + 1];
+    snprintf(digits, sizeof(digits), "%04d", num);
     fb_write_ascii(s, col, row, slice_view((uint8_t*)digits, sizeof(digits) - 1));
 }
+
+static void draw_stat(slice_mutable_t fb, uint8_t row, const char* label, uint32_t value) {
+    size_t label_len = strlen(label);
+    fb_write_ascii(fb, 0, row, slice_view((const uint8_t*)label, label_len));
+
+    const uint8_t number_stat_col = 64;
+    write_number(fb, number_stat_col, row, value);
+}
+
