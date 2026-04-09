c646eb267c04aa7137ff90e8cd67e37fbf770302
atkvo <voandrew.khoa@gmail.com>
2026-04-08 22:33:48 -0700

    refactor(app): simplify button task state machine

--- app/inc/task_button.h
+++ app/inc/task_button.h
@@ -1,23 +1,22 @@
 #pragma once
 
 #include <stdint.h>
+#include "antos.h"
 #include "gpio.h"
 
 typedef enum {
     BTN_TASK_INIT = 0,
     BTN_TASK_WAIT_FOR_PRESS,
-    BTN_TASK_WAIT_DEBOUNCE,
-    BTN_TASK_CHECK_FINAL_STATE,
+    BTN_TASK_DEBOUNCE_CHECK,
 } btn_task_state_t;
 
 typedef struct {
     btn_task_state_t state;
     gpio_pin_handle_t button;
     bool last_state;
-    uint32_t last_pressed;
     uint32_t press_count;
 } button_task_ctx_t;
 
 button_task_ctx_t task_button_create_ctx(gpio_pin_handle_t btn);
-void task_read_button(button_task_ctx_t *ctx);
+ant_task_status_t task_read_button(button_task_ctx_t *ctx);
 
--- app/src/tasks/task_button.c
+++ app/src/tasks/task_button.c
@@ -3,20 +3,20 @@
 
 button_task_ctx_t task_button_create_ctx(gpio_pin_handle_t btn) {
     button_task_ctx_t button_ctx = { 0 };
+    button_ctx.state = BTN_TASK_INIT;
     button_ctx.button = btn;
     button_ctx.press_count = 0;
 
     return button_ctx;
 }
 
-void task_read_button(button_task_ctx_t *ctx) {
+ant_task_status_t task_read_button(button_task_ctx_t *ctx) {
     enum {
         DEBOUNCE_TIME = 80,
         IS_PRESSED = false,
-        IS_NOT_PRESSED = true,
     };
 
-    uint64_t elapsed_time = 0;
+    uint32_t next_deadline_ms = 5;
     switch (ctx->state) {
         case BTN_TASK_INIT:
             ctx->state = BTN_TASK_WAIT_FOR_PRESS;
@@ -24,24 +24,12 @@ void task_read_button(button_task_ctx_t *ctx) {
 
         case BTN_TASK_WAIT_FOR_PRESS:
             if (gpio_pin_read(ctx->button) == IS_PRESSED) {
-                ctx->state = BTN_TASK_WAIT_DEBOUNCE;
-                ctx->last_pressed = ant_get_tick_count();
+                ctx->state = BTN_TASK_DEBOUNCE_CHECK;
+                next_deadline_ms = DEBOUNCE_TIME;
             }
             break;
 
-        case BTN_TASK_WAIT_DEBOUNCE:
-            {
-                elapsed_time = ant_get_tick_count() - ctx->last_pressed;
-                if (elapsed_time < DEBOUNCE_TIME) {
-                    break;
-                }
-                else {
-                    ctx->state = BTN_TASK_CHECK_FINAL_STATE;
-                    /* fall through switch case */
-                }
-            }
-
-        case BTN_TASK_CHECK_FINAL_STATE:
+        case BTN_TASK_DEBOUNCE_CHECK:
             if (gpio_pin_read(ctx->button) == IS_PRESSED) {
                 ctx->press_count++;
             }
@@ -50,5 +38,7 @@ void task_read_button(button_task_ctx_t *ctx) {
             break;
     }
 
-    ant_task_schedule_next(5);
+    ant_task_schedule_next(next_deadline_ms);
+
+    return ANT_TASK_OK;
 }
